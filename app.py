1b82daab1af923360bce2b08722f1f6376e058e9
E02Y <elvis2002125@gmail.com>
2022-08-20 12:30:28 +0800

    Unified inputs + more descriptive docstrings

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def show_command_list():
     """
@@ -12,9 +13,9 @@ def show_command_list():
     """
     return (
         "<p>Commands</p>"
+        + "<p>Get one ticket : /get<idnum></p>"
         + "<p>List all tickets: /getall</p>"
         + "<p>Add/edit a ticket: /save</p>"
-        + "<p>Get one ticket : /get<idnum></p>"
     )
 
 
@@ -23,64 +24,73 @@ def get_all_tickets():
     """
     Returns a JSON containing all tickets as JSONs.
     """
-    if request.method != "GET":
-        return ("Wrong request method, try POST.", 400)
-    try:
-        with open("database.json", "r") as f:
-            db: dict = json.load(f)
-        return (db, 200)
-    except Exception:
-        return ("An error occurred😢.", 400)
+    with open("database.json", "r") as f:
+        db: dict = json.load(f)
+    return (db, 200)
 
 
 @app.route("/save", methods=["POST"])
 def save_ticket():
     """
     Give me a new/edited ticket(as JSON), I will add/change it to the database.
+    JSON Format: {
+        "idstr": "uuid",
+        "title": "title",
+        "description": "description",
+        "repository": "repo link",
+        "difficulty": "difficulty",
+        "assignee": "assignee",
+        "role": "role"
+    }
+    When making a new ticket, leave the "uuid" field as an empty string. 
+
     """
-    if request.method != "POST":
-        return ("Wrong request method, try POST.", 400)
 
     body: dict = request.get_json()
-    idstr = tuple(body.keys())[0]
+    idstr = body["idstr"]
+    status = "add"
 
     with open("database.json", "r") as f:
         db: dict = json.load(f)
 
-    if idstr in db:
-        db.update(body)
-        with open("database.json", "w") as f:
-            json.dump(db, f, indent=4)
-        return (f'Ticket updated, ticket id {idstr}', 200)
+    if idstr:
+        if idstr in db:
+            status = "updat"
+        else:
+            return (
+                f"Ticket id {idstr} is not found in the database. Do you want to create a new ticket?",
+                200,
+            )
     else:
         idstr = str(uuid4())
-        db.update(
-            {
-                idstr: {
-                    "contents": {
-                        "title": body["title"],
-                        "description": body["description"],
-                        "repo": body["repo"],
-                        "difficulty": body["difficulty"],
-                        "assignee": body["assignee"],
-                        "role": body["role"]
-                    },
-                    "time": str(datetime.now())
-                }
+
+    db.update(
+        {
+            idstr: {
+                "contents": {
+                    "title": body["title"],
+                    "description": body["description"],
+                    "repository": body["repo"],
+                    "difficulty": body["difficulty"],
+                    "assignee": body["assignee"],
+                    "role": body["role"],
+                },
+                "last updated": str(datetime.now()),
             }
-        )
-        with open("database.json", "w") as f:
-            json.dump(db, f, indent=4)
-        return (f'New ticket added, ticket id {idstr}.', 200)
-        
+        }
+    )
+    with open("database.json", "w") as f:
+        json.dump(db, f, indent=4)
+    return (f"Ticket id {idstr} has been {status}ed.", 200)
 
 
-@app.route("/get<idnum>", methods=["GET"])
+@app.route("/get<idstr>", methods=["GET"])
 def get_one(idstr):
+    """
+    Give me a uuid, I will return the corresponding ticket JSON.
+    """
     with open("database.json", "r") as f:
         db: dict = json.load(f)
     ticket = db.get(idstr)
-    if ticket:
-        return (ticket, 200)
-    else:
-        return ('Ticket not found', 200)
+
+    return (ticket, 200) if ticket else ("Ticket not found", 200)
