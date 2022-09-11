ea731469ac77ec7691d310c38669a82129061e67
E02Y <elvis2002125@gmail.com>
2022-09-11 18:57:10 +0800

    more functions yay

--- db.py
+++ db.py
@@ -8,15 +8,15 @@ extras.register_uuid()
 TICKETSDB_TABLES = ['tickets', 'roles', 'ticket_role']
 
 
-def get_connection_cursor(fn):
+def get_conn_cursor(fn):
     def wrapper(*args, **kwargs):
         with psycopg2.connect(dbname='ticketing_system', user='postgres', password='1234') as conn:
             with conn.cursor() as cur:
-                fn(cur, *args, **kwargs)
+                return fn(cur, *args, **kwargs)
     return wrapper
 
 
-@get_connection_cursor
+@get_conn_cursor
 def init_db(cur):
     # tickets table
     cur.execute('''
@@ -37,9 +37,8 @@ def init_db(cur):
     # roles table
     cur.execute('''
         CREATE TABLE IF NOT EXISTS roles (
-            id SERIAL,
-                PRIMARY KEY (id),
-            name TEXT UNIQUE NOT NULL,
+            name TEXT,
+                PRIMARY KEY (name),
             description TEXT NOT NULL
         );
     ''')
@@ -47,21 +46,19 @@ def init_db(cur):
     # tickets roles intermediate
     cur.execute('''
         CREATE TABLE IF NOT EXISTS ticket_role (
-            id SERIAL,
-                PRIMARY KEY (id),
             ticket_id UUID,
-            role_id INT,
+            role_name TEXT,
             CONSTRAINT fk_ticket
                 FOREIGN KEY (ticket_id)
                     REFERENCES tickets(id),
             CONSTRAINT fk_role
-                FOREIGN KEY (role_id)
-                    REFERENCES roles(id)
+                FOREIGN KEY (role_name)
+                    REFERENCES roles(name)
         );
     ''')
 
 
-@get_connection_cursor
+@get_conn_cursor
 def yeet_db(cur):
     for table in TICKETSDB_TABLES:
         cur.execute(sql.SQL('DROP TABLE IF EXISTS {} CASCADE;').format(sql.Identifier(table)))
@@ -72,34 +69,74 @@ def reset_db():
     init_db()
 
 
-@get_connection_cursor
+@get_conn_cursor
 def add_ticket(cur, ticket: dict):
     roles = ticket.pop('roles')
     cur.execute('INSERT INTO tickets VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);', tuple(ticket.values()))
 
     for role in roles:
-        cur.execute("SELECT role_id FROM roles WHERE name = (%s);", (role,))
-        role_id = cur.fetchone()
-        cur.execute("""
-            INSERT INTO ticket_role (ticket_id, role_id) VALUES (%s);""", (ticket['id'], *role_id))
+        cur.execute("INSERT INTO ticket_role VALUES (%s, %s);", (ticket['id'], role))
+
+if False:
+    add_ticket({
+    "id": uuid.uuid4(),
+    "title": "Easiest task ever",
+    "description": "Do stuff",
+    "repository": "N/A",
+    "skills": "N/A",
+    "difficulty": "Dark Souls No Hit",
+    "assignee": "idy",
+    "roles": ["frontend", "backend"],
+    "last_modified": datetime.datetime.fromisoformat("2022-08-23 10:04:09.104992"),
+    "created_at": datetime.datetime.fromisoformat("2022-08-20 10:04:09.104992")
+}
+)
+
+@get_conn_cursor
+def get_ticket(cur, id: uuid.UUID) -> dict:
+    cur.execute('SELECT * FROM tickets WHERE id = %s;', (id,))
+    columns = [desc[0] for desc in cur.description]
+    values = cur.fetchone()
+    ticket = dict(zip(columns, values))
+
+    cur.execute('''
+        SELECT name 
+        FROM roles
+            INNER JOIN ticket_role 
+            ON ticket_role.role_name = roles.name
+        WHERE ticket_role.ticket_id = %s;''', (id,))
+    roles = cur.fetchall()
+    ticket['roles'] = [role[0] for role in roles]
+
+    return ticket
+
+@get_conn_cursor
+def yeet_ticket(cur, id: uuid.UUID):
+    cur.execute('DELETE FROM tickets WHERE id = %s;', (id,))
+    cur.execute('DELETE FROM ticket_role WHERE ticket_id = %s;', (id,))
 
 
-@get_connection_cursor
+@get_conn_cursor
 def add_role(cur, name: str, desc: str):
-    cur.execute('INSERT INTO roles (name, description) VALUES (%s, %s);', (name, desc))
+    cur.execute('INSERT INTO roles VALUES (%s, %s);', (name, desc))
 
 
-@get_connection_cursor
+def add_cur_roles():
+    add_role('frontend', '')
+    add_role('backend', '')
+
+
+@get_conn_cursor
 def test1(cur, name):
-    cur.execute("""INSERT INTO roles (name, description) VALUES (%s, %s);""", (name, 'bottom test'))
+    cur.execute("INSERT INTO roles VALUES (%s, %s);", (name, 'it is a role'))
 
-@get_connection_cursor
+@get_conn_cursor
 def test2(cur):
     cur.execute("""SELECT * FROM roles;""")
-    print(cur.fetchone())
-    print(cur.fetchone())
+    print(cur.fetchall())
 
 
 if __name__ == "__main__":
     ...
     # reset_db()
+    # print(get_ticket(uuid.UUID('c3da3f10-36f2-4c01-a151-6a19c4243d1f')))
